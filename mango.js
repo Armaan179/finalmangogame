5afe9fc7f97c83a8c316ce7f3907721a8db5e6f1
Armaan179 <67940150+Armaan179@users.noreply.github.com>
2021-01-15 17:32:49 -0800

    Add files via upload

---
+++ mango.js
@@ -0,0 +1,19 @@
+class Mango{
+    constructor(x,y,radius){
+        var options={
+            isStatic:true,
+            restitution:0,
+            friction:1,
+
+        }
+        this.body=Bodies.circle(x, y, radius/2, options)
+        this.radius=radius;
+        this.image=loadImage("mango.png");
+        World.add(world,this.body)
+    }
+    display(){
+        imageMode(CENTER)
+        image(this.image,this.body.position.x, this.body.position.y,this.radius,this.radius);
+
+    }
+}
